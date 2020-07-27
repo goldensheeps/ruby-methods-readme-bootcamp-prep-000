826e8547b8af84769fb4a51f5aad65040b6dfebd
Timothy Lalli <trlalli92@gmail.com>
2020-07-27 16:35:50 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,5 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
 
 10.times do 
   puts phrase
